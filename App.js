ac2fcdabb05b3111fd89eddbc18f76e6eabd5ca9
wsabir <wsabir@devsolutions.team>
2024-05-14 00:08:30 +0500

    lecture 2 pushed

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-const h1 =  React.createElement('h1', {id: 'h1', className:'asdjasjdajs'}, 'Hello World From React!')
-const h2 =  React.createElement('h2', {id: 'h2', className:'asdjasjdajs'}, 'Hello World From React!')
-const parent = React.createElement('div',{id: 'parent'}, React.createElement('div', {id: 'child'}, [h1, h2]))
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const h1 = React.createElement('h1', { id: 'h1', className: 'asdjasjdajs' }, 'Slaasssm From React!')
+const h2 = React.createElement('h2', { id: 'h2', className: 'asdjasjdajs' }, 'Hello World From React!')
+const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [h1, h2]))
 const root = ReactDOM.createRoot(document.getElementById('root'))
 console.log(root);
 root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Namste React sdsdsd
+# Namste React 
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement      
+- File Watching Algorithm - written in C++
+- Cashing - Faster Builds
+- Image Optimization
+- Mininfication
+- Bundling
+- Compressing Files
+- Code Splitting
+- Constant Hashing
+- Differential Bundling - support older browsers
+- Diagnostics
+- Tree Shaking
